b50206363b6f5d07d96556bd24223b034c7715a7
Ganesh Maind <2022wb86985@wilp.bits-pilani.ac.in>
2026-09-25 15:40:17 +0530

    Modify source code for CI demonstration

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("DevOps Lab 2 - Continuous Integration using Jenkins and Maven");
         System.out.println("BITS ID: 2022wb86985");
+        System.out.println("Source code modified for Continuous Integration demonstration.");
     }
 }
