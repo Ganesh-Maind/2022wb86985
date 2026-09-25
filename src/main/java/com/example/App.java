ecb3d0963716c96259e58b50a9f761e5cfc6f1a3
Ganesh Maind <2022wb86985@wilp.bits-pilani.ac.in>
2026-09-25 14:44:30 +0530

    Add Maven project for DevOps Lab 2

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,8 @@
+package com.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("DevOps Lab 2 - Continuous Integration using Jenkins and Maven");
+        System.out.println("BITS ID: 2022wb86985");
+    }
+}
